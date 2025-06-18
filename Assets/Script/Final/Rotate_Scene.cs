45e56c17f5310d63c37300c04e3d036f493296ed
Zetossover <francisco.romero.molina@zonavirtual.uisek.cl>
2025-06-18 12:12:21 -0400

    Fco: 18-6-25

--- Assets/Script/Final/Rotate_Scene.cs
+++ Assets/Script/Final/Rotate_Scene.cs
@@ -4,16 +4,42 @@ public class Rotate_Scene : MonoBehaviour
 {
     public Transform scene;
     public float rotateSpd;
+    public bool rotateLeft;
+    public bool rotateRight;
+    public int rotateCountLeft = 0;
+    public int rotateCountRight = 0;
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && rotateLeft == true)
         {
             RotateLeft();
+            rotateCountLeft++;
+            rotateCountRight--;
         }
-        if (Input.GetMouseButton(1))
+        if (Input.GetMouseButton(1) && rotateRight == true)
         {
             RotateRight();
+            rotateCountRight++;
+            rotateCountLeft--;
+        }
+        if (rotateCountLeft == 0)
+        {
+            rotateCountLeft = 0;
+            rotateLeft = true;
+        }
+        if (rotateCountRight == 0)
+        {
+            rotateCountRight = 0;
+            rotateRight = true;
+        }
+        if (rotateCountLeft >= 70)
+        {
+            rotateLeft = false;
+        }
+        if (rotateCountRight >= 70)
+        {
+            rotateRight = false;
         }
     }
     void RotateLeft()
@@ -22,6 +48,6 @@ public class Rotate_Scene : MonoBehaviour
     }
     void RotateRight()
     {
-
+        scene.Rotate(0, 0, -Time.deltaTime * rotateSpd);
     }
 }
