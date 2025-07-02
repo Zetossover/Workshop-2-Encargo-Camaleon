21469bdb32e33fb8e0024a0c5da59fa43fa2996c
Monsalve-Nicolas <nicolas.monsalve@zonavirtual.uisek.cl>
2025-07-02 09:46:30 -0400

    Avances nivel final
    assets del nivel final

--- Assets/Script/Precipitacion/ContadorGotitas.cs
+++ Assets/Script/Precipitacion/ContadorGotitas.cs
@@ -6,21 +6,33 @@ public class ContadorGotitas : MonoBehaviour
 {
     [SerializeField] int gotitasMaximas = 40, contadorGotitas;
     [SerializeField] Image barraDeAgua;
+    [SerializeField] float probabilidad;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         contadorGotitas = 0;
         barraDeAgua.fillAmount = 0f;
+        probabilidad = Random.Range(0, 100);
     }
     void Update()
     {
         NextScene();
+        
     }
     void NextScene()
     {
         if(contadorGotitas >= gotitasMaximas)
         {
-            SceneManager.LoadScene("Final");
+            if(probabilidad >= 50f)
+            {
+                SceneManager.LoadScene("Final");
+                Debug.Log("Scene_Final");
+            }
+            else
+            {
+                SceneManager.LoadScene("Final_Alternativo");
+            }
+            
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
