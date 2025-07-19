65856733fe968c74ab5c9993e9df0c23ee09ee34
Zetossover <172427581+Zetossover@users.noreply.github.com>
2025-07-19 07:05:57 -0400

    Ajustes Final Alternativo

--- Assets/Script/Final/Rotate_Scene.cs
+++ Assets/Script/Final/Rotate_Scene.cs
@@ -3,51 +3,22 @@ using UnityEngine;
 public class Rotate_Scene : MonoBehaviour
 {
     public Transform scene;
-    public float rotateSpd;
-    public bool rotateLeft;
-    public bool rotateRight;
-    public int rotateCountLeft = 0;
-    public int rotateCountRight = 0;
+    public float rotateSpd = 100f;
+    public float maxAngle = 70f;
 
     private void Update()
     {
-        if (Input.GetMouseButton(0) && rotateLeft == true)
+        float currentZ = scene.localEulerAngles.z;
+        if (currentZ > 180f) currentZ -= 360f; // Convertimos de 0-360 a -180 a 180
+
+        if (Input.GetMouseButton(0) && currentZ < maxAngle)
         {
-            RotateLeft();
-            rotateCountLeft++;
-            rotateCountRight--;
+            scene.Rotate(0, 0, Time.deltaTime * rotateSpd);
         }
-        if (Input.GetMouseButton(1) && rotateRight == true)
+
+        if (Input.GetMouseButton(1) && currentZ > -maxAngle)
         {
-            RotateRight();
-            rotateCountRight++;
-            rotateCountLeft--;
+            scene.Rotate(0, 0, -Time.deltaTime * rotateSpd);
         }
-        if (rotateCountLeft == 0)
-        {
-            rotateCountLeft = 0;
-            rotateLeft = true;
-        }
-        if (rotateCountRight == 0)
-        {
-            rotateCountRight = 0;
-            rotateRight = true;
-        }
-        if (rotateCountLeft >= 70)
-        {
-            rotateLeft = false;
-        }
-        if (rotateCountRight >= 70)
-        {
-            rotateRight = false;
-        }
-    }
-    void RotateLeft()
-    {
-        scene.Rotate(0, 0, Time.deltaTime * rotateSpd);
-    }
-    void RotateRight()
-    {
-        scene.Rotate(0, 0, -Time.deltaTime * rotateSpd);
     }
 }
